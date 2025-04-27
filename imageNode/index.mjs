9f085f485e83e00a4dcf13ee00db17d28dc46383
albelfio <5812464+Belfio@users.noreply.github.com>
2025-04-27 17:08:22 +0100

    clean task repo

--- imageNode/index.mjs
+++
@@ -1,6 +0,0 @@
-import { Resource } from "sst";
-
-for (let i = 0; i < 10; i++) {
-  console.log(`NEW: The bucket name is ${Resource.MyBucket.name}`);
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-}
